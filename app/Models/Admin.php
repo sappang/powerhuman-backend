dd597de011bf53c0c9cbde0fefca11e64392ce16
sappang <dsappang@gmail.com>
2023-09-12 15:36:02 +0700

    tambahan table admin dan role

--- app/Models/Admin.php
+++ app/Models/Admin.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Admin extends Model
@@ -19,4 +20,9 @@ class Admin extends Model
         'company_id',
         'is_aktif'
     ];
+
+    public function role(): BelongsTo
+    {
+        return $this->belongsTo(Role::class);
+    }
 }
