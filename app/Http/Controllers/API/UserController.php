3f946d9143548bc39c25612cb87a52f41f1d2f3c
sappang <dsappang@gmail.com>
2023-09-15 16:24:47 +0700

    postman login documentation

--- app/Http/Controllers/API/UserController.php
+++ app/Http/Controllers/API/UserController.php
@@ -2,10 +2,15 @@
 
 namespace App\Http\Controllers\API;
 
+use App\Helpers\ResponseFormatter;
 use App\Http\Controllers\Controller;
+use App\Models\User;
 use Exception;
+use GuzzleHttp\Psr7\Message;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
+use Laravel\Fortify\Rules\Password;
 
 class UserController extends Controller
 {
@@ -19,18 +24,78 @@ class UserController extends Controller
                 'password' => 'required'
             ]);
     
-            // TODO : FIND USER BY EMAIL
+            // TODO: FIND USER BY EMAIL
             
-            $credential = $request(['email','password']);
-            if (Auth::attempt($credential)) {
-                
+            $credentials = request(['email','password']);
+            if (!Auth::attempt($credentials)) 
+            {
+                return ResponseFormatter::error(['Unauthorized'],401);
             }
-            // TODO : GENERATE TOKEN
-            
-            // TODO : RETURN RESPONSE
-        } catch (Exception $e) {
+                
+            $user = User::where('email',$request->email)->first();
+                
+
+            if (!Hash::check($request->password, $user->password)) 
+            {
+                throw new Exception('Invalid Password');
+            }
+            // TODO: GENERATE TOKEN
+            $tokenresult = $user->createToken('authToken')->plainTextToken;
+                
+            // TODO: RETURN RESPONSE
+            return ResponseFormatter::success([
+                'access_token' => $tokenresult,
+                'token_type' => 'Bearer',
+                'user' => $user
+            ],'Login Sukses');
+        } catch (Exception $error) {
             //throw $th;
+            return ResponseFormatter::error('Authentification Failed');
         }
+
+    }
+    public function register(Request $request)
+    {
+        try {
+            //TODO: VALIDATE REQUEST
+            $request->validate([
+                'name' => ['required','string','max:255'],
+                'email' => ['required','string','email','max:255','unique:users'],
+                'password' => ['required','string',new Password],
+            ]);
+            //TODO: CREATE USER
+            $user = User::create([
+                'name' => $request->name,
+                'email' => $request->email,
+                'password' => Hash::make($request->password)
+            ]);
+            //TODO: GENERATE TOKEN
+            $tokenresult = $user->createToken('authToken')->plainTextToken;
+            //TODO  : RETURN RESPONSE
+            return ResponseFormatter::success([
+                'access_token' => $tokenresult,
+                'token_type' => 'Bearer',
+                'user' => $user
+            ],'Register Sukses');
+
+        } catch (Exception $error) {
+            //TODO: RETURN ERROR RESPONSE
+            return ResponseFormatter::error('Register Gagal');
+        }
+    }
+    public function logout(Request $request)
+    {
+        // TODO: REVOKE TOKEN
+        $token = $request->user()->currentAccessToken()->delete();
+        // TODO: RETURN RESPONSE
+        return ResponseFormatter::success($token,'logout sukses');
+    }
+    public function fetch(Request $request)
+    {
+        // TODO: GET USER
+        $user = $request->user();
+        // TODO: RETURN RESPONSE
+        return ResponseFormatter::success($user,'Fetch berhasil');
 
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\API\CompanyController;
+use App\Http\Controllers\API\UserController;
 use App\Models\Company;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
@@ -16,8 +17,11 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
-});
-
 Route::get('/company',[CompanyController::class, 'all']);
+
+Route::post('login',[UserController::class,'login']);
+Route::post('register',[UserController::class,'register']);
+Route::post('logout',[UserController::class,'logout']);
+Route::get('fetch',[UserController::class,'fetch'])->middleware('auth:sanctum');
+
+
